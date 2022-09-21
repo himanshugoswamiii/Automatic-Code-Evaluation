4e2cfb42946f3f9b9a9b2afc346753ee9372a3ef
Himanshu <himanshugoswami.12feb@gmail.com>
2022-09-21 22:05:29 +0530

    Partially Completed Evaluate page

--- Automatic_Code_Evaluation/application/evaluate/evaluator.py
+++ Automatic_Code_Evaluation/application/evaluate/evaluator.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from pathlib import Path
+import os
 from django.http import HttpResponse
 import requests as req
 import application.testing.main
+
+BASE_DIR = Path(__file__).resolve().parent.parent.parent # Modify no of parents according to this file's location
 
 def code_submit(request):
     return render(request, "submit_code.html")
@@ -10,27 +14,26 @@ def form(request):
     return render(request,"testing.html")
 
 
+# To upload the file
 def executor(request):
     teacher = request.FILES['teachercode']
     student = request.FILES['student']
     testcase = request.FILES['testcase']
-    basedir = "/home/aniket/PycharmProjects/Automatic-Code-Evaluation"
-    with open(basedir + "/" + str(teacher.name), 'wb+') as dest:
+    basedir = os.path.join(BASE_DIR,'media/')
+    #basedir = "/home/himanshu/Desktop/Automatic-Code-Evaluation/Automatic_Code_Evaluation/media"
+    # with open(basedir + "/" + str(teacher.name), 'wb+') as dest:
+    with open(basedir + str(teacher.name), 'wb+') as dest:
         for ch in teacher.chunks():
             dest.write(ch)
-    with open(basedir + "/" + str(testcase.name), 'wb+') as dest:
+    # with open(basedir + "/" + str(testcase.name), 'wb+') as dest:
+    with open(basedir + str(testcase.name), 'wb+') as dest:
         for ch in testcase.chunks():
             dest.write(ch)
-    with open(basedir + "/" + str(student.name), 'wb+') as dest:
+    # with open(basedir + "/" + str(student.name), 'wb+') as dest:
+    with open(basedir + str(student.name), 'wb+') as dest:
         for ch in student.chunks():
             dest.write(ch)
     return HttpResponse("Done")
-
-
-
-
-
-
 
 def contact(request):
     return render(request, 'contact.html')
